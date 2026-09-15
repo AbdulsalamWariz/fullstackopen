6d16070e057ebf044cecdef1471cb50024461016
AbdulsalamWariz <abdulsalamwariz06@gmail.com>
2026-09-15 14:40:41 +0100

    Verified Front-End and Backend Integration

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,30 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
-
-const password = process.argv[2]
-const url = `mongodb+srv://abdulsalamwariz06_db_user:${password}@cluster0.wookfbc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url, { family: 4 })
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
@@ -55,13 +36,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -85,18 +62,17 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
