0fa4b6d4f43eafe23b85756e2a9b1c779d34f144
AbdulsalamWariz <abdulsalamwariz06@gmail.com>
2026-09-15 14:14:02 +0100

    Progress: -Exercise 3.12 -Connecting the backend to a database

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -1,36 +1,60 @@
-const express = require("express")
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json())
-app.use(express.static("dist"))
+app.use(express.static('dist'))
+
+const password = process.argv[2]
+const url = `mongodb+srv://abdulsalamwariz06_db_user:${password}@cluster0.wookfbc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
-    id: "1",
-    content: "HTML is easy",
+    id: '1',
+    content: 'HTML is easy',
     important: true,
   },
   {
-    id: "2",
-    content: "Browser can execute only JavaScript",
+    id: '2',
+    content: 'Browser can execute only JavaScript',
     important: false,
   },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
     important: true,
   },
 ]
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>")
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World</h1>')
 })
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
   if (note) {
@@ -40,7 +64,7 @@ app.get("/api/notes/:id", (request, response) => {
   }
 })
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter(note => note.id !== id)
 
@@ -52,12 +76,12 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body) {
     return response.status(400).json({
-      error: "content is missing",
+      error: 'content is missing',
     })
   }
 
---
+++ part3/notes_backend/mongo.js
@@ -0,0 +1,48 @@
+// const dns = require('dns')
+// dns.setDefaultResultOrder('ipv4first')
+// dns.setServers(['8.8.8.8', '1.1.1.1'])
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://abdulsalamwariz06_db_user:${password}@cluster0.wookfbc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/*
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+/*
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+*/
