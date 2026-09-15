736206c73c305739c47a33dbf87b67a50a21af9d
AbdulsalamWariz <abdulsalamwariz06@gmail.com>
2026-09-15 15:24:37 +0100

    Completed Exercises 3.13 and 3.14

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,16 +1,20 @@
-const express = require("express")
-const morgan = require("morgan")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method)
-  console.log("Path:  ", request.path)
-  console.log("Body:  ", request.body)
-  console.log("---")
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
   next()
 }
 
 app.use(express.json())
+app.use(express.static('dist'))
 // app.use(requestLogger)
 app.use(
   morgan(function (tokens, req, res) {
@@ -18,35 +22,35 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       JSON.stringify(req.body),
-    ].join(" ")
+    ].join(' ')
   }),
 )
 
 let persons = [
   {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
   },
 ]
 
@@ -54,69 +58,48 @@ const info = `
 <p>Phonebook has info for ${persons.length} people</p>
 <p>${new Date()}</p>
 `
-// const alreadyExists = name => {
-//   name = persons.find(person => person.name === name)
-//   console.log(name)
-// }
-const alreadyExists = name => {
-  return !!persons.find(
-    person => person.name.toLowerCase() === name.toLowerCase(),
-  )
-}
 
-console.log(alreadyExists("Arto Hellas"))
-
-app.get("/api/persons", (request, response) => {
-  response.status(200).json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   response.status(200).send(info)
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id
-
-  const person = persons.find(person => person.id === id)
-
-  if (!person) {
-    return response.status(404).json({
-      error: "Resource not found",
-    })
-  }
-  response.status(200).json(person)
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then(returnedPerson => {
+    response.json(returnedPerson)
+  })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => String(person.id) !== id)
   response.status(204).end()
 })
 
-app.post("/api/persons", (request, response) => {
+app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(422).json({ error: "The name or number is missing" })
+    return response.status(422).json({ error: 'The name or number is missing' })
   }
 
-  if (alreadyExists(body.name)) {
-    return response.status(409).json({ error: "name must be unique" })
-  }
-
-  const person = {
-    id: Math.floor(Math.random() * 1000000000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.status(201).json(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
